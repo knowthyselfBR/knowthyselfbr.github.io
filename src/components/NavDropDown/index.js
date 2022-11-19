fad2898f521b3a78815f1f582a0d7a309a04d892
Rafael Duarte <rd.dev.2022@gmail.com>
2022-11-19 19:46:24 -0300

    update

---
+++ src/components/NavDropDown/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AiFillPhone, AiOutlineMail } from 'react-icons/ai'
+import { FaLocationArrow } from "react-icons/fa"
+
+import { DropDownContainer, DropDownIcon, DropDownItem, DropDownItemDesc, DropDownItemTitle, DropDownTextContainer } from './NavDropDown'
+
+const NavDropDown = (props) => (
+  <div>
+    
+    <AiFillPhone />
+    <AiOutlineMail />
+    <FaLocationArrow />
+    <DropDownContainer /> 
+    <DropDownIcon />
+  </div>
+);
+
+export default NavDropDown
