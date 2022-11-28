9b2b55fe4e3d30c16491d2815214b6124586ff32
Rafael Duarte <rd.dev.2022@gmail.com>
2022-11-27 22:35:53 -0300

    Upgrade of the portfolio webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-function App() {
+import MainContent from "./components/MainContent";
+import Sidebar from "./components/Sidebar";
 
+import "./styles/components/app.sass";
+
+function App() {
   return (
-    <div className="App">
-      <h1>Hi there@</h1>
-      
+    <div id="portfolio">
+      <Sidebar />
+      <MainContent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,18 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>About</h2>
+      <p>
+        I'm Rafael Duarte, web developer, currently in love with React apps.
+
+        In regard to education, I'm a bachelor of information systems, with expected graduation for December 2023.
+
+      </p>
+      <p>
+        Besides the knowledge with the Tech below, I'm a blockchain development enthusiast and looking forward to work on projects in this niche in the future.
+      </p>
+    </section>
+  );
+};
+
+export default AboutContainer;
