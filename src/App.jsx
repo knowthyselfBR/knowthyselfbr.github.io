0d4104792859dab8c4d99ebb371c0ab20eae1168
Rafael Duarte <rd.dev.2022@gmail.com>
2023-01-09 21:00:20 -0300

    Update to Sass only, no framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import MainContent from "./components/MainContent";
-import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
-
 import "./styles/components/app.sass";
 
 
 function App() {
   return (
-    <>
-      <Navbar />
       <div id='portfolio'>
         <Sidebar />
         <MainContent />
       </div>
-      <Footer />
-    </>
   );
 }
 
--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -3,7 +3,6 @@ import {
   DiCss3,
   DiJsBadge,
   DiNodejsSmall,
-  DiMysql,
   DiReact,
   DiSass,
 } from "react-icons/di";
@@ -11,12 +10,12 @@ import {
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
-  { id: "html", name: "HTML5", icon: <DiHtml5 />, description: "Creating HTML elements with React" },
-  { id: "css", name: "CSS3", icon: <DiCss3 />, description: "Final form of style after the usage of Sass for more efficient work" },
-  { id: "sass", name: "Sass", icon: <DiSass />, description: "Developing great CSS with frameworks (Bootstrap, Bulma etc.) and this preprocessor option" },
-  { id: "js", name: "JavaScript", icon: <DiJsBadge />, description: "Main programing language of choice for these kind of projects" },
-  { id: "node", name: "Node.js", icon: <DiNodejsSmall />, description: "Back-end JavaScript runtime environment to execute JavaScript code outside a web browser" },
-  { id: "react", name: "React", icon: <DiReact />, description: "JavaScript library for building user interfaces based on components" },
+  { id: "html", name: "HTML5", icon: <DiHtml5 /> },
+  { id: "css", name: "CSS3", icon: <DiCss3 /> },
+  { id: "sass", name: "Sass", icon: <DiSass /> },
+  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
+  { id: "react", name: "React", icon: <DiReact /> },
 ];
 
 const TechnologiesContainer = () => {
@@ -29,7 +28,6 @@ const TechnologiesContainer = () => {
             {tech.icon}
             <div className="technology-info">
               <h3>{tech.name}</h3>
-              <p>{tech.description}</p>
             </div>
           </div>
         ))}
